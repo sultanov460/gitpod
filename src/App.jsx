dd498f9ac98043c336efd9c473eb509d5b84e334
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2025-07-22 20:56:58 +0400

    Finish think section and begin remote section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { Hero } from "./components/Hero/Hero";
 import { Select } from "./components/Select/Select";
+import { Think } from "./components/Think/Think";
 
 function App() {
   return (
     <div>
       <Hero />
       <Select />
+      <Think />
     </div>
   );
 }
---
+++ src/components/Think/Think.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import s from "./style.module.scss";
+
+export const Think = () => {
+  return (
+    <>
+      <div className="container">
+        <div className={s.wrapper}>
+          <div className={s.textBox}>
+            <h2>Think CI/CD for dev environments</h2>
+            <p>
+              We invented prebuilds so application code, configuration and
+              infrastructure can all be stored as machine-executable code in
+              your git repositories and applied to dev environments
+              automatically and continuously.
+            </p>
+            <div className={s.btns}>
+              <button className={s.btnPrebuilds}>More on prebuilds</button>
+              <button className={s.btnDocumentation}>Documentation</button>
+            </div>
+          </div>
+          <div className={s.image}>
+            <img src="think-section.png" alt="think-section" />
+          </div>
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
